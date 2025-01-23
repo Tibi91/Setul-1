42f72dc93e37028add607bc26517541ab4ef9466
Tibi91 <vargatibike120@gmail.com>
2025-01-23 09:24:10 +0200

    Set-1-1

---
+++ Set-1-1/Set-1-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] S = Console.ReadLine().Split(' ');
+            double a = double.Parse(S[0]);
+            double b = double.Parse(S[1]);
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Ecuatia nu poate fi rezolvata");
+                }
+                else
+                {
+                    Console.WriteLine("x = 0");
+                }
+            }
+            else
+            {
+                double x = -b / a;
+                Console.WriteLine("x = " + x); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
